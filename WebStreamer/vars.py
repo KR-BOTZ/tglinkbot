5e8b519d8a843812036631816c1a4f0ea959cc0d
Gaurav-4x <117690973+Gaurav-4x@users.noreply.github.com>
2022-12-18 14:01:43 +0530

    Update vars.py

--- WebStreamer/vars.py
+++ WebStreamer/vars.py
@@ -7,15 +7,15 @@ load_dotenv()
 
 
 class Var(object):
-    API_ID = int(getenv("API_ID", "13956112"))
-    API_HASH = str(getenv("API_HASH", "a14d6d445c6be0a22f4a5ad401e0eab6"))
-    BOT_TOKEN = str(getenv("BOT_TOKEN", "5412074036:AAGiYcP16KrKdnd1nb4o0VNUed7HqlH4M5g"))
-    SESSION_NAME = str(getenv('SESSION_NAME', 'Moksh_b658'))
+    API_ID = int(getenv("API_ID", "22161709"))
+    API_HASH = str(getenv("API_HASH", "c1292ced60651fe3670854e31c7d2785"))
+    BOT_TOKEN = str(getenv("BOT_TOKEN", "5884742619:AAGti_x1h4H4xMiWAytCLl-Dnlq8N3ceSL4"))
+    SESSION_NAME = str(getenv('SESSION_NAME', 'gdchekobot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv("BIN_CHANNEL", "-1001713974897"))
+    BIN_CHANNEL = int(getenv("BIN_CHANNEL", "-1001584785114"))
     PORT = int(getenv('PORT', 8080))
-    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'filetolink4x.co.in'))
+    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     OWNER_ID = int(getenv('OWNER_ID', '2062513342'))
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
@@ -31,4 +31,4 @@ class Var(object):
     DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://manju:1234@cluster0.s6qpf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'))
     PING_INTERVAL = int(getenv('PING_INTERVAL', '500'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
-    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001296894100")).split()))
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001584785114")).split()))

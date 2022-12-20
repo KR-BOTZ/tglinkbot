9984624a033091f38ab00e947a2fb64ec2fd4b3a
KR-BOTZ <119985743+KR-BOTZ@users.noreply.github.com>
2022-12-20 09:15:04 +0530

    Update vars.py

--- WebStreamer/vars.py
+++ WebStreamer/vars.py
@@ -7,28 +7,28 @@ load_dotenv()
 
 
 class Var(object):
-    API_ID = int(getenv("API_ID", "22161709"))
-    API_HASH = str(getenv("API_HASH", "c1292ced60651fe3670854e31c7d2785"))
-    BOT_TOKEN = str(getenv("BOT_TOKEN", "5884742619:AAGti_x1h4H4xMiWAytCLl-Dnlq8N3ceSL4"))
-    SESSION_NAME = str(getenv('SESSION_NAME', 'gdchekobot'))
+    API_ID = int(getenv('API_ID', '13384432'))
+    API_HASH = str(getenv('API_HASH','ea9db4503ed7088b788e06dfd818e00e'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN'))
+    SESSION_NAME = str(getenv('SESSION_NAME', 'KRFile2linkBot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv("BIN_CHANNEL", "-1001584785114"))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001777601172'))
     PORT = int(getenv('PORT', 8080))
-    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'filetolink4x.co.in'))
-    OWNER_ID = int(getenv('OWNER_ID', '2062513342'))
+    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
+    OWNER_ID = int(getenv('OWNER_ID', '1504797855'))
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    environ["FQDN"] = "filetolink4x.co.in"
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
     else:
         ON_HEROKU = False
-    FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
+    FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.onrender.com'
     URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else \
-        "https://{}:{}/".format(FQDN, PORT)
-    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://manju:1234@cluster0.s6qpf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'))
+        "http://{}:{}/".format(FQDN, PORT)
+    DATABASE_URL = str(getenv('DATABASE_URL','mongodb+srv://SK:SK@cluster0.1b5gk3x.mongodb.net/?retryWrites=true&w=majority'))
     PING_INTERVAL = int(getenv('PING_INTERVAL', '500'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
-    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001584785114")).split()))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'KR_BOTZ'))
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
+

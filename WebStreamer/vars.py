2650e45648d06455560b7403156318ddc06a2276
KR-BOTZ <119985743+KR-BOTZ@users.noreply.github.com>
2022-12-20 22:19:31 +0530

    Update vars.py

--- WebStreamer/vars.py
+++ WebStreamer/vars.py
@@ -7,16 +7,16 @@ load_dotenv()
 
 
 class Var(object):
-    API_ID = int(getenv('API_ID', '13384432'))
-    API_HASH = str(getenv('API_HASH','ea9db4503ed7088b788e06dfd818e00e'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN', '5516869519:AAH92zxWwuGJhngQEd1fHndl4oIYubFanxQ'))
-    SESSION_NAME = str(getenv('SESSION_NAME', 'KRFile2linkBot'))
+    API_ID = int(getenv('API_ID'))
+    API_HASH = str(getenv('API_HASH'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN'))
+    SESSION_NAME = str(getenv('SESSION_NAME', 'AviStreamBot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001777601172'))
-    PORT = int(getenv('PORT', '8080'))
-    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'kr-link-gq.onrender.com'))
-    OWNER_ID = int(getenv('OWNER_ID', '1504797855'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL'))
+    PORT = int(getenv('PORT', 8080))
+    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
+    OWNER_ID = int(getenv('OWNER_ID', '797848243'))
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
     if 'DYNO' in environ:
@@ -24,11 +24,10 @@ class Var(object):
         APP_NAME = str(getenv('APP_NAME'))
     else:
         ON_HEROKU = False
-    FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.onrender.com'
+    FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else \
-        "https://{}:{}/".format(FQDN, PORT)
-    DATABASE_URL = str(getenv('DATABASE_URL','mongodb+srv://SK:SK@cluster0.1b5gk3x.mongodb.net/?retryWrites=true&w=majority'))
+        "http://{}:{}/".format(FQDN, PORT)
+    DATABASE_URL = str(getenv('DATABASE_URL'))
     PING_INTERVAL = int(getenv('PING_INTERVAL', '500'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'KR_BOTZ'))
-    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
-
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001296894100")).split()))

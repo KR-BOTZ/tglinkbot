bd87d0adc0f96d107644cef11ad58e9a9e284936
KR-BOTZ <119985743+KR-BOTZ@users.noreply.github.com>
2022-12-20 09:36:11 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -74,19 +74,16 @@ async def cb_data(bot, update):
     if update.data == "home":
         await update.message.edit_text(
             text=Config.START_TEXT.format(update.from_user.mention),
-            disable_web_page_preview=True,
             reply_markup=START_BUTTONS
         )
     elif update.data == "help":
         await update.message.edit_text(
             text=Config.HELP_TEXT,
-            disable_web_page_preview=True,
             reply_markup=HELP_BUTTONS
         )
     elif update.data == "about":
         await update.message.edit_text(
             text=Config.ABOUT_TEXT,
-            disable_web_page_preview=True,
             reply_markup=ABOUT_BUTTONS
         )
     elif update.data == "dev":
